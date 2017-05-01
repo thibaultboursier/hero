7fd02dbd4c3ec6c0ba948eac389a1cbcff1d751f
boursier robin <boursier robin>
2017-05-01 13:51:27 +0200

    adding hero service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,24 @@
-import {Component} from '@angular/core';
-
-class Hero {
-  id: number;
-  name: string;
-}
-
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
+﻿import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1 [hidden]="!hero">{{title}}</h1>
-    
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div>
-        <label>id: </label>{{selectedHero.id}}
-      </div>
-      <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-    
+    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
-    <ul>
-      <li *ngFor="let hero of heroes;let i = index" (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}} / {{i}}
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <app-hero-detail [hero]="selectedHero"></app-hero-detail>
 `,
+  providers: [
+      HeroService
+  ],
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -93,12 +71,21 @@ const HEROES: Hero[] = [
 })
 export class AppComponent {
   title: string = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) {
+      
+  }
+
+  getHeroes() {
+      this.heroService.getHeroes()
+          .then((heroes:Hero[]) => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+      this.getHeroes();
+  }
 
   /**
    * On select event.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import {NgModule}      from '@angular/core';
+﻿import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
-import {AppComponent}  from './app.component';
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.components';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [
+      AppComponent,
+      HeroDetailComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
